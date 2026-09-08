ad34a3f08926f5345bff85ad8ec730a72873c523
ur-kgy <0000kgy@gmail.com>
2026-09-08 16:25:12 +0900

    5. api 명세서 대로 필드, 생성자 만들기, Validation 적용하기 완료

--- src/main/java/com/gamebasic/runcard/dto/RunCardRequest.java
+++ src/main/java/com/gamebasic/runcard/dto/RunCardRequest.java
@@ -1,10 +1,23 @@
 package com.gamebasic.runcard.dto;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 
 @Getter
 public class RunCardRequest {
     // TODO (Lv 5): API 명세의 카드 필드 제약을 Bean Validation 어노테이션으로 붙이세요.
+    @NotEmpty
     private String cardType;
+
+    @Max(10)
+    @Min(0)
     private Integer acquiredFloor;
+
+    public RunCardRequest(String cardType, Integer acquiredFloor) {
+        this.cardType = cardType;
+        this.acquiredFloor = acquiredFloor;
+    }
 }
